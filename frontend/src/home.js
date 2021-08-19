0942ccd2a485b224a741ddc35a06bef317cab440
Sahil Vasava <sahil@atliq.com>
2021-08-19 19:06:30 +0530

    STL: changed ui

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -9,52 +9,117 @@ import Container from "@material-ui/core/Container";
 import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import { Paper, CardActionArea, CardMedia, CardActions, Grid, IconButton } from "@material-ui/core";
+// import { MuiThemeProvider, createMuiTheme } from '@material-ui/styles'
+// import { ThemeProvider } from '@material-ui/styles';
+// import { createMuiTheme } from "@material-ui/core/styles";
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import AddAPhoto from '@material-ui/icons/AddAPhoto';
+import Image from '@material-ui/icons/Image';
 import cblogo from "./cblogo.PNG";
+import image from "./bg.png";
+import leaf from "./leaf.jpg";
+import { height } from "@material-ui/system";
+
 
 const axios = require("axios").default;
 
-const useStyles = makeStyles({
+// const whiteTheme = createMuiTheme({ palette: { primary: white } })
+
+const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
   },
-  center: {
-    marginTop: "3vh",
-    width: "60%",
+  // center: {
+  //   marginTop: "3vh",
+  //   width: "60%",
+  // },
+  // bottom: {
+  //   margin: "auto",
+  //   width: "50%",
+  // },
+  // previewText: {
+  //   width: "100%",
+  //   marginTop: "20px",
+  // },
+  // imgPreview: {
+  //   textAlign: "center",
+  //   margin: "5px 15px",
+  //   height: "400px",
+  //   // width: "100%",
+  //   borderLeft: "1px solid gray",
+  //   borderRight: "1px solid gray",
+  //   borderTop: "5px solid gray",
+  //   borderBottom: "5px solid",
+  // },
+  // img: {
+  //   display: "block",
+  //   marginLeft: "auto",
+  //   marginRight: "auto",
+  //   width: "50%",
+  // },
+  // card: {
+  //   marginTop: "3vh",
+  //   border: "1px solid black",
+  //   marginBottom: "3vh",
+  // },
+  // uploadButton: {
+  //   marginTop: "20px",
+  // },
+  root: {
+    maxWidth: 345,
+    flexGrow: 1,
   },
-  bottom: {
+  media: {
+    height: 400,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  paperContainer: {
+    // backgroundImage: `url(${Image})`,
+    height: "inherit",
+    flexGrow: 1,
+  },
+  gridContainer: {
+    flexGrow: 1,
+    height: 'inherit',
+    justifyContent: "center",
+  },
+  mainContainer: {
+    backgroundImage: `url(${image})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: "93vh",
+  },
+  imageCard: {
     margin: "auto",
-    width: "50%",
+    width: 400,
+    height: 500,
+    backgroundColor: 'transparent',
+    // marginTop: "3vh",
+    //   border: "1px solid black",
+    //   marginBottom: "3vh",
   },
-  previewText: {
-    width: "100%",
-    marginTop: "20px",
+  noImage: {
+    margin: "auto",
+    width: 400,
+    height: "400 !important",
   },
-  imgPreview: {
-    textAlign: "center",
-    margin: "5px 15px",
-    height: "400px",
-    width: "100%",
-    borderLeft: "1px solid gray",
-    borderRight: "1px solid gray",
-    borderTop: "5px solid gray",
-    borderBottom: "5px solid",
+  input: {
+    display: 'none',
   },
-  img: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "50%",
+  uploadIcon: {
+    background: 'white',
   },
-  card: {
-    marginTop: "3vh",
-    border: "1px solid black",
-    marginBottom: "3vh",
-  },
-  uploadButton: {
-    marginTop: "20px",
-  },
-});
-
+  //   detail {
+  //   flex: 1,
+  //   justifyContent: ''
+  // }
+}));
 export const ImageUpload = () => {
   const classes = useStyles();
   const [selectedFile, setSelectedFile] = useState();
@@ -71,6 +136,13 @@ export const ImageUpload = () => {
     setPreview(objectUrl);
   }, [selectedFile]);
 
+  useEffect(() => {
+    if (!image) {
+      return;
+    }
+    sendFile();
+  }, [preview]);
+
   const onSelectFile = (e) => {
     if (!e.target.files || e.target.files.length === 0) {
       setSelectedFile(undefined);
@@ -82,15 +154,19 @@ export const ImageUpload = () => {
     setData(undefined);
     setImage(true);
   };
-  async function sendFile() {
+  const sendFile = async () => {
+    console.log('hiya', image);
     if (image) {
+      console.log('hiya');
       let formData = new FormData();
       formData.append("file", selectedFile);
+      console.log('here', selectedFile);
       let res = await axios({
         method: "post",
         url: process.env.REACT_APP_API_URL,
         data: formData,
       });
+      console.log('hiya', res);
       if (res.status === 200) {
         setData(res.data);
       }
@@ -112,35 +188,86 @@ export const ImageUpload = () => {
           <Avatar src={cblogo}></Avatar>
         </Toolbar>
       </AppBar>
-      <Container className={classes.center}>
-        {!image && (
-          <div className={classes.imgPreview}>
-            <div className={classes.previewText}>
-              Please select an Image to Process
-            </div>
-          </div>
-        )}
-        {image && <img src={preview} className={classes.img} />}
-        <br />
-        <div className={classes.bottom}>
-          <input type="file" onChange={onSelectFile} /><br/>
-          <Button
-            onClick={sendFile}
-            variant="contained"
-            color="primary"
-            className={classes.uploadButton}
-          >
-            Process
-          </Button>
-          {data && (
-            <Card className={classes.card}>
-              <CardContent>
-                <p>Condition: {data.class}</p>
-                <p>Confidence: {confidence}%</p>
-              </CardContent>
+      <Container maxWidth={false} className={classes.mainContainer} disableGutters={true}>
+        <Grid
+          className={classes.gridContainer}
+          container
+          direction="row"
+          // justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <Card className={classes.imageCard}>
+              {image && <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={preview}
+                  component="image"
+                  title="Contemplative Reptile"
+                />
+              </CardActionArea>
+              }
+              {!image && <CardContent className={classes.content}>
+                <Typography component="h5" variant="h5">
+                  Please select an Image to Process
+                </Typography>
+              </CardContent>}
+              {data && <CardContent className={classes.detail}>
+                <Typography variant="span">Condition     </Typography>
+                <Typography variant="span">Confidence</Typography>
+                {/* <Typography variant="h5">Confidence: {confidence}%</Typography> */}
+                {/* <Typography variant="h5">Condition: {data.class}</Typography> */}
+                <Typography variant="span">{data.class} </Typography>
+                <Typography variant="span">{confidence}%</Typography>
+
+              </CardContent>}
             </Card>
-          )}
-        </div>
+          </Grid>
+          <Grid item>
+            <input accept="image/*" className={classes.input} id="icon-button-file" type="file" onChange={onSelectFile} />
+            <label htmlFor="icon-button-file">
+              {/* <ThemeProvider theme={whiteTheme}> */}
+
+              <Button variant="contained" className={classes.uploadButton} color="primary" component="span" size="large" startIcon={<Image fontSize="large" onClick={sendFile} />}>
+                Upload
+              </Button>
+              {/* </ThemeProvider> */}
+            </label>
+          </Grid>
+        </Grid>
+        {/* </Paper> */}
+        {/* </div> */}
+        {/* {!image && (
+              <div className={classes.imgPreview}>
+                <div className={classes.previewText}>
+                  Please select an Image to Process
+                </div>
+              </div>
+            )}
+            {image && <img src={preview} className={classes.img} />}
+            <br />
+            <div className={classes.bottom}>
+              <input type="file" onChange={onSelectFile} /><br />
+              <Button
+                onClick={sendFile}
+                variant="contained"
+                color="primary"
+                className={classes.uploadButton}
+              >
+                Process
+              </Button>
+              {data && (
+                <Card className={classes.card}>
+                  <CardContent>
+                    <p>Condition: {data.class}</p>
+                    <p>Confidence: {confidence}%</p>
+                  </CardContent>
+                </Card>
+              )}
+            </div> */}
+
+        {/* </Paper> */}
+        {/* </Paper> */}
       </Container>
     </React.Fragment>
   );
